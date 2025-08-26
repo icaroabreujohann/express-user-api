9f333c94dc76f6ba871202e00d37f1cdeae8a2f4
icarosvj <icaroabreujohann01@gmail.com>
2025-08-26 13:50:34 -0300

    initial project setup
    chore: start api with express, postgres, dotenv and cors
    
    configure database connection and express base

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const postgres = require('postgres')
+
+const sql = postgres(process.env.DATABASE_URL)
+
+const testConnectionDb = async () => {
+   try {
+      const response = await sql`select now()`
+      console.log(`Connect to database was successful:`, response)
+   } catch (error) {
+      console.log('Connect to database failed', error)
+   }
+}
+
+testConnectionDb()
+
+module.exports = {sql, testConnectionDb}
